1375af87ef3253356f3cb59131f654ae82f178e3
zhangzhen1979 <zhen.zhang@163.com>
2021-12-22 16:31:12 +0800

    加入Nacos支持

--- src/main/java/com/thinkdifferent/convertvideo/config/ConvertVideoConfig.java
+++ src/main/java/com/thinkdifferent/convertvideo/config/ConvertVideoConfig.java
@@ -1,21 +1,17 @@
 package com.thinkdifferent.convertvideo.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RefreshScope
 public class ConvertVideoConfig {
 
+    @Value(value = "${convert.video.outPutPath}")
     public static String outPutPath;
-    @Value("${convert.video.outPutPath}")
-    public void setOutPutPath(String strOutPutPath) {
-        ConvertVideoConfig.outPutPath = strOutPutPath;
-    }
 
+    @Value(value = "${convert.video.ffmpegPath}")
     public static String ffmpegPath;
-    @Value("${convert.video.ffmpegPath}")
-    public void setFfmpegPath(String strFfmpegPath) {
-        ConvertVideoConfig.ffmpegPath = strFfmpegPath;
-    }
 
 }
